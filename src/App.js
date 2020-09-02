d81e5e5b63af54e2a0dcf9488a37dbdebbcb5a6a
soban123 <sobanshams88@gmail.com>
2020-09-03 00:02:41 +0500

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
+import React , {useEffect , useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   const [id, setId] = useState(1);
+   const [topic, setTopic] = useState('');
+
+  useEffect(()=>{
+
+    let id = 2 ; 
+
+    const myquery =  `
+    query getcourse($id : Int! ){
+      course( id:$id  ){
+        id
+        title
+      }
+    }
+    `
+    
+  
+
+    fetch('http://localhost:4000/graphql' , {
+      headers : { 'Content-Type' : 'application/json' } , 
+      method : 'post' ,
+      body : JSON.stringify(
+        {
+          query : myquery ,
+          variables : { id  }
+
+        }
+      )
+    })
+    .then( res => res.json() )
+    .then( data => console.log(data) )
+  } , [])
+
+  const handleSubmit = () =>{
+    const myquery =  `
+    mutation getcourse($id : Int! , $topic: String! ){
+      updateTopic( id:$id  , topic : $topic ){
+        id
+        title
+      }
+    }
+    `
+    
+  
+
+    fetch('http://localhost:4000/graphql' , {
+      headers : { 'Content-Type' : 'application/json' } , 
+      method : 'post' ,
+      body : JSON.stringify(
+        {
+          query : myquery ,
+          variables : { id , topic  }
+
+        }
+      )
+    })
+    .then( res => res.json() )
+    .then( data => console.log(data.data.updateTopic) )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Change topic  </h2>
+      <input value = { id } onChange={ e => setId(+(e.target.value))  } />
+      <input value={topic}  onChange={ e => setTopic(e.target.value)  } />
+      <button onClick={handleSubmit}> Submit </button>
     </div>
   );
 }
